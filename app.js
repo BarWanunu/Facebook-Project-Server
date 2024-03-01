f3ff92e651f93d704f0fd5ed7942d6010d5f688b
shakedg1234 <shaked.gitta@gmail.com>
2024-03-01 14:00:07 +0200

    add patch

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 const app = express();
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-res.send('<html><body><h1>GET</h1></body></html>')
-})
+import cors from 'cors';
+app.use(express.static('public'));
+app.use(cors());
+// Middleware to parse JSON data
+app.use(bodyParser.json());
+
+// Route to handle the POST or PATCH request
+app.post('/login', (req, res) => {
+  const { displayName, password } = req.body;
+  if (displayName == 'guest' && password == 'Aa12345678') {
+    
+    res.json({ message: 'Authentication successful' });
+  }
+  else{
+    return res.status(401).json({ message: 'Invalid username or password' });
+
+  }
+
+});
 app.listen(80)

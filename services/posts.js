d5888d930ef073dd971c58e2c0b894680f74320b
Itay Chananel <itay17283@gmail.com>
2024-05-08 18:16:51 +0300

    connection to TCP

--- services/posts.js
+++ services/posts.js
@@ -3,9 +3,11 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/users');
 const tokenSevice= require('../services/token.js');
 // Function to create a new post
+const net = require('net');
 async function createPost(text, token, date, img) {
     const secretKey = 'yourSecretKey';
-        try {
+    const urlRegex = /(https?:\/\/www\.|https?:\/\/|www\.)?[a-zA-Z0-9]{2,}(\.[a-zA-Z0-9]{2,})+(\.[a-zA-Z0-9]{2,})?/g; // Regular expression to match URLs 
+    try {
         // Verify and decode the received token
         const decoded = jwt.verify(token, secretKey);
 
@@ -22,14 +24,24 @@ async function createPost(text, token, date, img) {
         const userProfile = user.photo;
         const lastPost = await Post.findOne({}, {}, { sort: { 'id': -1 } });
         const newPostId = lastPost ? lastPost.id + 1 : 1;
-        if(img){
-               // Check if photo size is within 140 KB
-               const sizeInKB = Buffer.byteLength(img, 'base64') / 1024;
-               if (sizeInKB > 1023) {
-                   return { success: false, message: 'Post image size exceeds the limit of 1023 KB' };
-               }
-        }
+        const hasUrl = urlRegex.test(text);
 
+        // Print to console if there is a URL in the text
+        if (hasUrl) {
+            console.log('The text contains a URL:', text);
+    
+            // Extract the URL from the text
+            const url = text.match(urlRegex)[0]; // Assuming there's only one URL in the text
+    
+            // Check the URL
+            const isURLAllowed = await checkURL(url);
+            if (!isURLAllowed) {
+                // URL is not allowed, return an error message
+                return { success: false, message: 'The URL is not allowed' };
+            }
+        
+        }
+    
         // Create a new post
         const post = new Post({
             id:newPostId,
@@ -52,6 +64,44 @@ async function createPost(text, token, date, img) {
         console.error('Error creating post:', error.message);
         return { success: false, message: 'An error occurred while creating the post' };
     }
+}
+async function checkURL(url) {
+    return new Promise((resolve, reject) => {
+        const client = new net.Socket();
+        const TCP_SERVER_HOST =process.env.TCP_SERVER_IP;
+        const TCP_SERVER_PORT = process.env.TCP_SERVER_PORT;
+
+        client.connect(TCP_SERVER_PORT, TCP_SERVER_HOST, () => {
+            console.log('Connected to TCP server');
+            // Send "2 " followed by the URL to the server
+            client.write(`2 ${url}\n`);
+        });
+
+        // Listen for data from the server
+        client.on('data', (data) => {
+            console.log('Received data from server:', data.toString());
+            const receivedData = data.toString(); // Convert data buffer to string
+            if (receivedData === "true true") {
+                resolve(false); // Resolve with true if the URL is allowed
+            } else {
+                resolve(true); // Resolve with false if the URL is not allowed
+            }
+            // Process the received data here
+        });
+
+        // Listen for errors
+        client.on('error', (error) => {
+            console.error('Error:', error.message);
+            reject(error); // Reject the Promise if there's an error
+            // Handle errors here
+        });
+
+        // Listen for the connection to close
+        client.on('close', () => {
+            console.log('Connection closed');
+            // Handle connection close here
+        });
+    });
 }
 // Function to get all posts for user's fees
 async function getAllPosts(token) {
